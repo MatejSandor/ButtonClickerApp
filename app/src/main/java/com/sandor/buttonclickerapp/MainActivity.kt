b0c6285844aa01216306962ae63e8a1ad7480211
Matej Šándor <matejko.sandor@gmail.com>
2020-04-19 18:18:49 +0200

    added simple button functionality

--- app/src/main/java/com/sandor/buttonclickerapp/MainActivity.kt
+++ app/src/main/java/com/sandor/buttonclickerapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.sandor.buttonclickerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -10,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private var userInput: EditText? = null
     private var button: Button? = null
     private var textView: TextView? = null
+    private var numTimesClicked = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +21,19 @@ class MainActivity : AppCompatActivity() {
         userInput = findViewById<EditText>(R.id.editText)
         button = findViewById<Button>(R.id.button)
         textView = findViewById<TextView>(R.id.textView)
+        textView?.text = ""
+        textView?.movementMethod = ScrollingMovementMethod()
+
+        button?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                numTimesClicked += 1
+                textView?.append("The button was clicked $numTimesClicked time")
+                if (numTimesClicked != 1) {
+                    textView?.append("s \n")
+                } else {
+                    textView?.append("\n")
+                }
+            }
+        })
     }
 }
