31da67c7dcaef71f82150813cd7ea2dcb4e919ab
Matej Šándor <matejko.sandor@gmail.com>
2020-04-20 11:44:31 +0200

    saving and restoring instance state

--- app/src/main/java/com/sandor/buttonclickerapp/MainActivity.kt
+++ app/src/main/java/com/sandor/buttonclickerapp/MainActivity.kt
@@ -9,13 +9,14 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
-private val TAG = "MainActivity"
+private const val TAG = "MainActivity"
+private const val TEXT_CONTENTS = "TextContent"
 
 class MainActivity : AppCompatActivity() {
     private var textView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG,"onCreate: called")
+        Log.d(TAG, "onCreate: called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -28,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
-                Log.d(TAG,"onClick: called")
+                Log.d(TAG, "onClick: called")
                 textView?.append("${userInput.text} \n")
                 userInput.text.clear()
             }
@@ -36,42 +37,44 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onStart() {
-        Log.d(TAG,"onStart: called")
+        Log.d(TAG, "onStart: called")
         super.onStart()
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        Log.d(TAG,"onRestoreInstanceState: called")
+        Log.d(TAG, "onRestoreInstanceState: called")
         super.onRestoreInstanceState(savedInstanceState)
+        textView?.text = savedInstanceState.getString(TEXT_CONTENTS, "")
     }
 
     override fun onResume() {
-        Log.d(TAG,"onResume: called")
+        Log.d(TAG, "onResume: called")
         super.onResume()
     }
 
     override fun onPause() {
-        Log.d(TAG,"onPause: called")
+        Log.d(TAG, "onPause: called")
         super.onPause()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        Log.d(TAG,"onSaveInstanceState: called")
+        Log.d(TAG, "onSaveInstanceState: called")
         super.onSaveInstanceState(outState)
+        outState.putString(TEXT_CONTENTS, textView?.text.toString())
     }
 
     override fun onStop() {
-        Log.d(TAG,"onStop: called")
+        Log.d(TAG, "onStop: called")
         super.onStop()
     }
 
     override fun onRestart() {
-        Log.d(TAG,"onRestart: called")
+        Log.d(TAG, "onRestart: called")
         super.onRestart()
     }
 
     override fun onDestroy() {
-        Log.d(TAG,"onDestroy: called")
+        Log.d(TAG, "onDestroy: called")
         super.onDestroy()
     }
 
