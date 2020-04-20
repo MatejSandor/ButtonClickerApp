db275f817860e1953b845a231d9450bd3c4bcdf6
Matej Šándor <matejko.sandor@gmail.com>
2020-04-20 10:59:25 +0200

    added activity lifecycle methods

--- app/src/main/java/com/sandor/buttonclickerapp/MainActivity.kt
+++ app/src/main/java/com/sandor/buttonclickerapp/MainActivity.kt
@@ -34,4 +34,45 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
+    override fun onStart() {
+        Log.d(TAG,"onStart: called")
+        super.onStart()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(TAG,"onRestoreInstanceState: called")
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onResume() {
+        Log.d(TAG,"onResume: called")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG,"onPause: called")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(TAG,"onSaveInstanceState: called")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        Log.d(TAG,"onStop: called")
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG,"onRestart: called")
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG,"onDestroy: called")
+        super.onDestroy()
+    }
+
 }
